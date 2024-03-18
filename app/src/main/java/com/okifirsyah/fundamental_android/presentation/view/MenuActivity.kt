a182fc336f4148ccb3e7f37346534fee13f357d1
Oki Firdaus Syah Putra <85375019+okifirsyah404@users.noreply.github.com>
2024-03-18 20:20:46 +0700

    Update search bar

--- app/src/main/java/com/okifirsyah/fundamental_android/presentation/view/MenuActivity.kt
+++ app/src/main/java/com/okifirsyah/fundamental_android/presentation/view/MenuActivity.kt
@@ -1,21 +1,41 @@
 package com.okifirsyah.fundamental_android.presentation.view
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.okifirsyah.fundamental_android.R
+import com.okifirsyah.fundamental_android.databinding.ActivityMainBinding
+import com.okifirsyah.fundamental_android.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMenuBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        _binding = ActivityMenuBinding.inflate(layoutInflater)
+
+        setContentView(_binding?.root)
+
+        with(_binding) {
+            this?.searchView?.setupWithSearchBar(searchBar)
+
+            this?.searchView
+                ?.editText
+                ?.setOnEditorActionListener { textView, actionId, event ->
+                    this.searchBar.setText(textView.text.toString())
+                    searchView.hide()
+                    Toast.makeText(this@MenuActivity, searchView.text, Toast.LENGTH_SHORT).show()
+                    false
+                }
         }
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
