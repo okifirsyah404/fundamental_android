ae99fbd51d7f647e672e156f0e0f89a98fe3e538
Oki Firdaus Syah Putra <85375019+okifirsyah404@users.noreply.github.com>
2024-03-18 20:00:39 +0700

    Complete Fragment Course

--- app/src/main/java/com/okifirsyah/fundamental_android/MainActivity.kt
+++ app/src/main/java/com/okifirsyah/fundamental_android/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.okifirsyah.fundamental_android
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.okifirsyah.fundamental_android.databinding.ActivityMainBinding
+import com.okifirsyah.fundamental_android.presentation.view.MenuActivity
+import com.okifirsyah.fundamental_android.presentation.view.MenuFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(_binding?.root)
+
+        _binding?.topAppBar?.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu1 -> {
+                    _binding?.fragmentContainer?.id?.let {
+                        supportFragmentManager.beginTransaction().replace(
+                            it, MenuFragment()).addToBackStack(null).commit()
+                    }
+                    true
+                }
+                R.id.menu2 -> {
+                    startActivity(Intent(this, MenuActivity::class.java))
+                    true
+                }
+                else -> false
+            }
         }
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
---
+++ app/src/main/java/com/okifirsyah/fundamental_android/presentation/view/MenuActivity.kt
@@ -0,0 +1,21 @@
+package com.okifirsyah.fundamental_android.presentation.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.okifirsyah.fundamental_android.R
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
