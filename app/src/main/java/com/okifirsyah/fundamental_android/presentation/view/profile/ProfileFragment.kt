af12fb1c789dbbca81eb410f12b928a50fbe1c9e
Oki Firdaus Syah Putra <85375019+okifirsyah404@users.noreply.github.com>
2024-03-19 06:43:29 +0700

    Update learn bottom nav

---
+++ app/src/main/java/com/okifirsyah/fundamental_android/presentation/view/profile/ProfileFragment.kt
@@ -0,0 +1,46 @@
+package com.okifirsyah.fundamental_android.presentation.view.profile
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.okifirsyah.fundamental_android.R
+import com.okifirsyah.fundamental_android.databinding.FragmentNotificationsBinding
+import com.okifirsyah.fundamental_android.databinding.FragmentProfileBinding
+import com.okifirsyah.fundamental_android.presentation.view.notifications.NotificationsViewModel
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val notificationsViewModel =
+            ViewModelProvider(this).get(ProfileViewModel::class.java)
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textNotifications
+        notificationsViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
